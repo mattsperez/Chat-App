8c66e0dc97759fc9695901414d524e01ffa77015
mattsperez <131983739+mattsperez@users.noreply.github.com>
2024-01-25 13:43:13 -0600

    5.4

--- chat-app/components/Chat.js
+++ chat-app/components/Chat.js
@@ -1,14 +1,24 @@
-import { StyleSheet, View, KeyboardAvoidingView, Platform } from 'react-native';
-import { useEffect, useState } from 'react';
-import { Bubble, GiftedChat } from "react-native-gifted-chat";
+import { StyleSheet, View, KeyboardAvoidingView, Platform} from "react-native";
+import { useEffect, useState } from "react";
+import { Bubble, GiftedChat, InputToolbar } from "react-native-gifted-chat";
 import { collection, query, addDoc, onSnapshot, orderBy } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Chat = ({ route, navigation, db }) => {
+
+const Chat = ({ route, navigation, db, isConnected }) => {
 
     const { name } = route.params;
     const { background } = route.params;
     const { userID } = route.params;
     const [messages, setMessages] = useState([]);
+
+
+    const loadCachedMessages = async () => {
+        const cachedMessages = (await AsyncStorage.getItem('messages')) || '[]';
+        setMessages(JSON.parse(cachedMessages));
+    };
+
+    let unsubMessages;
 
     const onSend = (newMessages) => { addDoc(collection(db, "messages"), newMessages[0]) }
 
@@ -16,29 +26,53 @@ const Chat = ({ route, navigation, db }) => {
         return <Bubble{...props}
             wrapperStyle={{
                 right: {
-                    backgroundColor: '#2a9d8f',
+                    backgroundColor: "#2a9d8f",
                 },
                 left: {
-                    backgroundColor: '#fff'
+                    backgroundColor: "#fff"
                 }
             }}
         />
     }
 
+    // prevents the text bar to render when offline
+    const renderInputToolbar = (props) => {
+        if (isConnected) return <InputToolbar {...props} />;
+        else return null;
+    }
+
     useEffect(() => {
         navigation.setOptions({ title: name });
-        const q = query(collection(db, "messages"), orderBy("createdAt", "desc"))
-        const unsubMessages = onSnapshot(q, (documentsSnapshot) => {
-            let newMessages = []
-            documentsSnapshot.forEach(doc => {
-                newMessages.push({ id: doc.id, ...doc.data(), createdAt: new Date(doc.data().createdAt.toMillis()) })
-            })
-            setMessages(newMessages)
-        })
+
+        if (isConnected === true) {
+            if (unsubMessages) unsubMessages();
+            unsubMessages = null;
+
+            const q = query(collection(db, 'messages'), orderBy('createdAt', 'desc'));
+            unsubMessages = onSnapshot(q, (documentSnapshot) => {
+                let newMessages = [];
+                documentSnapshot.forEach(doc => {
+                    newMessages.push({ id: doc.id, ...doc.data(), createdAt: new Date(doc.data().createdAt.toMillis()) })
+                });
+
+                cachedMessages(newMessages);
+                setMessages(newMessages);
+            });
+        } else loadCachedMessages();
+
+
         return () => {
             if (unsubMessages) unsubMessages()
         }
-    }, [])
+    }, []);
+
+    const cachedMessages = async (messagesToCache) => {
+        try {
+            await AsyncStorage.setItem("messages", JSON.stringify(messagesToCache));
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
 
     return (
         <View style={[styles.container, { backgroundColor: background }]}>
@@ -46,12 +80,13 @@ const Chat = ({ route, navigation, db }) => {
                 messages={messages}
                 renderBubble={renderBubble}
                 onSend={messages => onSend(messages)}
+                renderInputToolbar={renderInputToolbar}
                 user={{
                     _id: userID,
                     name: name
                 }}
             />
-            {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
+            {Platform.OS === "android" ? <KeyboardAvoidingView behavior="height" /> : null}
         </View>
     );
 }

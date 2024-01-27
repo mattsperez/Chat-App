35ae902b44af8d89e05380310496fd0af41b47fe
mattsperez <131983739+mattsperez@users.noreply.github.com>
2024-01-27 10:06:56 -0600

    5.5

--- chat-app/components/Chat.js
+++ chat-app/components/Chat.js
@@ -1,26 +1,17 @@
-import { StyleSheet, View, KeyboardAvoidingView, Platform} from "react-native";
+import { StyleSheet, View, KeyboardAvoidingView, Platform } from "react-native";
 import { useEffect, useState } from "react";
 import { Bubble, GiftedChat, InputToolbar } from "react-native-gifted-chat";
 import { collection, query, addDoc, onSnapshot, orderBy } from "firebase/firestore";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import CustomActions from "./CustomActions";
+import MapView from "react-native-maps";
 
-
-const Chat = ({ route, navigation, db, isConnected }) => {
+const Chat = ({ route, navigation, db, isConnected, storage }) => {
 
     const { name } = route.params;
     const { background } = route.params;
     const { userID } = route.params;
     const [messages, setMessages] = useState([]);
-
-
-    const loadCachedMessages = async () => {
-        const cachedMessages = (await AsyncStorage.getItem('messages')) || '[]';
-        setMessages(JSON.parse(cachedMessages));
-    };
-
-    let unsubMessages;
-
-    const onSend = (newMessages) => { addDoc(collection(db, "messages"), newMessages[0]) }
 
     const renderBubble = (props) => {
         return <Bubble{...props}
@@ -33,6 +24,33 @@ const Chat = ({ route, navigation, db, isConnected }) => {
                 }
             }}
         />
+    }
+
+    const renderCustomActions = (props) => {
+        return <CustomActions userID={userID} storage={storage} {...props} />;
+    };
+
+    const renderCustomView = (props) => {
+        const { currentMessage } = props;
+        if (currentMessage.location) {
+            return (
+                <MapView
+                    style={{
+                        width: 150,
+                        height: 100,
+                        borderRadius: 13,
+                        margin: 3
+                    }}
+                    region={{
+                        latitude: currentMessage.location.latitude,
+                        longitude: currentMessage.location.longitude,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                />
+            );
+        }
+        return null;
     }
 
     // prevents the text bar to render when offline
@@ -48,7 +66,7 @@ const Chat = ({ route, navigation, db, isConnected }) => {
             if (unsubMessages) unsubMessages();
             unsubMessages = null;
 
-            const q = query(collection(db, 'messages'), orderBy('createdAt', 'desc'));
+            const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
             unsubMessages = onSnapshot(q, (documentSnapshot) => {
                 let newMessages = [];
                 documentSnapshot.forEach(doc => {
@@ -74,12 +92,23 @@ const Chat = ({ route, navigation, db, isConnected }) => {
         }
     }
 
+    const loadCachedMessages = async () => {
+        const cachedMessages = (await AsyncStorage.getItem("messages")) || "[]";
+        setMessages(JSON.parse(cachedMessages));
+    };
+
+    let unsubMessages;
+
+    const onSend = (newMessages) => { addDoc(collection(db, "messages"), newMessages[0]) }
+
     return (
         <View style={[styles.container, { backgroundColor: background }]}>
             <GiftedChat
                 messages={messages}
                 renderBubble={renderBubble}
                 onSend={messages => onSend(messages)}
+                renderActions={renderCustomActions}
+                renderCustomView={renderCustomView}
                 renderInputToolbar={renderInputToolbar}
                 user={{
                     _id: userID,
